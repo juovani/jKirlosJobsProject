1a74058468245e11c64abf7fd8d016f0fc6fb5df
juovani <80779885+juovani@users.noreply.github.com>
2024-02-17 15:34:53 -0500

    got the data from the Excel file and adding it to new table in DB

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sqlite3
+import openpyxl
 from serpapi import GoogleSearch
 from secrets import apikey
 from typing import Tuple, List
@@ -32,6 +33,30 @@ def setup_db(cursor: sqlite3.Cursor):
     FOREIGN KEY (company_name) REFERENCES jobs (company_name)
     ON DELETE CASCADE ON UPDATE NO ACTION
     );''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS excel_data(
+        id INTEGER PRIMARY KEY,
+        company_name TEXT,
+        posting_age TEXT,
+        job_id TEXT,
+        country TEXT,
+        location TEXT,
+        publications TEXT,
+        salary_max TEXT,
+        salary_min TEXT,
+        salary_type TEXT,
+        job_title TEXT
+        );''')
+
+
+def make_initial_jobs_from_excel(cursor, excel_data):
+    for row in excel_data:
+        try:
+            cursor.execute('''INSERT INTO excel_data (company_name, posting_age, job_id, country, 
+            location, publications, salary_max, salary_min, csalary_type, job_title)
+                              VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', row)
+        except sqlite3.Error as e:
+            print("Error inserting Excel data:", e)
 
 
 def make_initial_jobs(cursor: sqlite3.Cursor, job_data: list):
@@ -86,19 +111,24 @@ def search_save(value, cursor):
         make_initial_jobs(cursor, job_data)
 
 
-# def get_multiple_pages_of_jobs(num_pages: int) -> List[tuple]:
-#     complete_data = []
-#     for page in range(num_pages):
-#         current_data = search_save(page)
-#         clean_data = clean_data_for_db(current_data)
-#         complete_data.extend(clean_data)
-#     return complete_data
+def read_excel_data(filename):
+    workbook = openpyxl.load_workbook(filename)
+    sheet = workbook.active
+
+    data = []
+    for row in sheet.iter_rows(values_only=True):
+        data.append(row)
+
+    return data
 
 
 def main():
     try:
         connection, cursor = open_db("your_database.db")
         setup_db(cursor)
+
+        excel_data = read_excel_data("Sprint3Data.xlsx")
+        make_initial_jobs_from_excel(cursor, excel_data)
 
         for value in range(1, 6):
             search_save(value, cursor)
